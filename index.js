97281d91d8b4e33e9ece2606048eff0c4d13cbe4
Androof <akopcz2@gmail.com>
2015-04-17 00:38:02 -0500

    er

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
-var WebSocketServer = require("ws").Server
+// var app = require('express')();
 
-var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
 
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// app.get('/', function(req, res){
+//   res.sendfile('index.html');
+// });
 
-var port = process.env.PORT || 5000
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+// });
 
-var server = http.createServer(app)
-server.listen(port)
+var express = require('express'),  
+    app = express.createServer(express.logger()),
+    io = require('socket.io').listen(app),
+    routes = require('./routes');
 
-var wss = new WebSocketServer({server: server})
+// Configuration
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+app.configure(function() {  
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function() {  
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function() {  
+  app.use(express.errorHandler());
+});
+
+// Heroku won't actually allow us to use WebSockets
+// so we have to setup polling instead.
+// https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
+io.configure(function () {  
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
+// Routes
+
+var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000  
+app.listen(port, function() {  
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
 
 io.on('connection', function(socket){
 
@@ -46,8 +76,10 @@ io.on('connection', function(socket){
 });
 
 
-http.listen(98.222.208.90, function(){
-  console.log('listening on *:3000');
-});
+
+
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
 
 
